1e0cadbd0dc68d53f7c4ec73048ce45077b4613c
Metin ATALAY <metin.atalay@architecht.com>
2022-03-13 13:21:28 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Metin Atalay on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
