a56f6bd684d5a300e107319b3df05a5c4d6c3592
Metin ATALAY <metin.atalay@architecht.com>
2022-03-13 19:16:31 +0300

    swift ui happy :)

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+           
+            //MARK : HEADER
+            Spacer()
+            
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 35: -35)
+                    .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
+            }
+            
+            Text("The time that leads to mastery is dependent is dependent on the intensity of our focus")
+                .font(.title3)
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                playSound(sound: "success", type: "m4a")
+               isOnboardingViewActive = true
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
+                
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            
+        } //: VStack
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                isAnimating = true
+            }
+        }
+        
     }
 }
 
--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -8,8 +8,171 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffeset: CGFloat = 0
+    @State private var isAnimating: Bool = false
+    @State private var imageOfset : CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var titleText: String = "Shared."
+    
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("ColorBlue").ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                //MARK: -HEADER
+                
+                Spacer()
+                
+                VStack(spacing:20) {
+                    Text(titleText)
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(titleText)
+                    
+                    Text("""
+                         It's not how much give but how much love we put into giving.
+                         """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal,10)
+                }
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
+                //MARK - Center
+                ZStack{
+                    
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOfset.width * -1)
+                        .blur(radius: abs(imageOfset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOfset)
+                    
+                    Image("character-1").resizable().scaledToFit()
+                        .opacity(isAnimating ? 1:0)
+                        .animation(.easeOut(duration: 1), value: isAnimating)
+                        .offset(x: imageOfset.width * 1.2 , y:0)
+                        .rotationEffect(.degrees(Double(imageOfset.width / 20)))
+                        .gesture(DragGesture()
+                                .onChanged({ gesture in
+                            
+                            if abs(imageOfset.width) <= 150 {
+                                imageOfset = gesture.translation
+                                
+                                withAnimation(.linear(duration: 0.25)){
+                                    indicatorOpacity = 0
+                                    titleText = "Give."
+                                }
+                            }
+                            
+                       
+                        })
+                                    .onEnded({ _ in
+                            imageOfset = .zero
+                            withAnimation(.linear(duration: 0.25)){
+                                indicatorOpacity = 1
+                                titleText = "Share."
+                            }
+                        })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOfset)
+                    
+                }
+                .overlay(
+                Image(systemName: "arrow.left.and.right.circle")
+                    .font(.system(size: 44, weight: .ultraLight))
+                    .foregroundColor(.white)
+                    .offset(y:40)
+                    .opacity(isAnimating ? 1: 0)
+                    .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                    .opacity(indicatorOpacity)
+                ,alignment: .bottom
+                )
+                
+                Spacer()
+                
+                //MARK: -Footer
+                
+                ZStack {
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule().fill(Color.white.opacity(0.2)).padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(.white)
+                        }.frame(width: 80, height: 80, alignment: .center)
+                            .offset(x: buttonOffeset)
+                            .gesture(DragGesture()
+                                        .onChanged({ gesture in
+                                if gesture.translation.width > 0 && buttonOffeset <= buttonWidth - 80 {
+                                    buttonOffeset = gesture.translation.width
+                                }
+                            })
+                                        .onEnded({ _ in
+                                withAnimation(Animation.easeOut(duration: 0.4)){
+                                    if buttonOffeset > buttonWidth / 2 {
+                                        hapticFeedback.notificationOccurred(.success)
+                                        playSound(sound: "chimeup", type: "mp3")
+                                        buttonOffeset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    }else {
+                                        hapticFeedback.notificationOccurred(.warning)
+                                        buttonOffeset = 0
+                                    }
+                                }
+                            })
+                            )
+                        
+                        
+                        Spacer()
+                    }
+                    
+                    
+                }.frame( height: 80, alignment: .center)
+                    .padding()
+                    .opacity(isAnimating ? 1: 0)
+                    .offset(y: isAnimating ? 0 :40)
+                    .animation(.easeOut(duration: 1), value: isAnimating)
+                
+            }
+        }.onAppear {
+            isAnimating = true
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
