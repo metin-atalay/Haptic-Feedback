ad67a537594954c2e960cea35d7c8edeb5ccbdb1
Metin ATALAY <metin.atalay@architecht.com>
2022-03-13 19:16:08 +0300

    first commit,

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Metin Atalay on 13.03.2022.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by Metin Atalay on 13.03.2022.
+//
+
+import Foundation
